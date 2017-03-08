e1d795021c05d130074a5f9c28ff52521d3d821e
vagrant <vladimir.grigore@gmail.com>
2017-03-08 20:55:33 +0000

    Add comments

--- characterPositioning.js
+++ characterPositioning.js
@@ -4,10 +4,16 @@ function countLetters(stringParameter) {
   var objectReturned = {};
 
   for(letter of stringParameter) {
+      //First check if the key already exists
       if (!objectReturned[letter]){
+        //If not, initialize key/value pair
         objectReturned[letter] = [stringParameter.indexOf(letter)];
       } else {
+        //Store the last value in the array for the current key
         var lastIndex = objectReturned[letter][objectReturned[letter].length-1];
+
+        //Use lastIndex value in indexOf function so that it doesn't return the
+        //first index again
         objectReturned[letter].push(stringParameter.indexOf(letter, lastIndex + 1));
       }
     }
